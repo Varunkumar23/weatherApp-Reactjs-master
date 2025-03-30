c37d5be276f0b307f4b21147ace12c75e9f0a648
Varunkumar23 <sarabuvarunkumar@gmail.com>
2025-03-30 16:22:06 +0530

    Initialize weather app with React, add essential files and configurations

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5769384dbf2bfaa9bdef1333b569cacd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
